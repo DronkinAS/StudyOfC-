1994ccbdaa345d4f2244b756268df5aba0d184a8
DronkinAS <111636044+DronkinAS@users.noreply.github.com>
2022-12-27 14:44:38 +0400

    A little arithmetic
    Added an example with elementary arithmetic operations

--- Study_Input_Output.cs
+++ Study_Input_Output.cs
@@ -2,10 +2,31 @@ using System;
 
 public class MainClass
 {
+    //Основной метод
     public static void Main()
     {
-         Console.WriteLine("Как тебя зовут?");
-         var text = Console.ReadLine();
-         Console.WriteLine("Привет, " + text); //Работает с русским текстом, но не с английским
+        Hello();
+        Arithmetic();
+    }
+    //Приветствие
+    public static void Hello(){
+        Console.WriteLine("Как тебя зовут?");
+        var text = Console.ReadLine();
+        Console.WriteLine("Привет, " + text); //Работает с русским текстом, но не с английским
+    }
+    //Арифметические операции
+    public static void Arithmetic()
+    {
+        Console.WriteLine("Поработаем с числами!");
+        Console.WriteLine("Введи первое число");
+        var A = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введи второе число");
+        var B = double.Parse(Console.ReadLine());
+        Console.WriteLine($"\nА = {A}, B = {B}");
+        Console.WriteLine($"A + B = {A + B}");
+        Console.WriteLine($"A - B = {A - B}");
+        Console.WriteLine($"A * B = {A * B}");
+        Console.WriteLine($"A / B = {A / B}");
+        Console.WriteLine($"A % B = {A % B}");
     }
 }
