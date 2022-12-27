1cb80d86783ba103b0c3acc1ac5f7efa38265ce1
DronkinAS <111636044+DronkinAS@users.noreply.github.com>
2022-12-27 15:20:12 +0400

    Convert from DEC to HEX
    Added the function of converting to a hexadecimal system

--- Study_Input_Output.cs
+++ Study_Input_Output.cs
@@ -5,8 +5,11 @@ public class MainClass
     //Основной метод
     public static void Main()
     {
-        Hello();
-        Arithmetic();
+        //Hello();
+        //Arithmetic();
+        Console.WriteLine("Проверяем преобразование из демятичной ситемы в шестнадцатиричную");
+        int decNumber = 256;
+        Console.WriteLine($"Исходное число: {decNumber}, в шестнадцатиричной: {decimalToHex(decNumber)}");        
     }
     //Приветствие
     public static void Hello(){
@@ -28,5 +31,27 @@ public class MainClass
         Console.WriteLine($"A * B = {A * B}");
         Console.WriteLine($"A / B = {A / B}");
         Console.WriteLine($"A % B = {A % B}");
+    }
+    //Преобрахование мз 10-ой в 16-ую
+    public static string decimalToHex(int decNumber)
+    {
+        string hexabc = "0123456789abcdef";
+        string result = "";
+        
+        while(true)
+        {
+            if (decNumber/16 >= 1)
+            {
+                result = hexabc[decNumber % 16] + result;
+                decNumber /= 16;
+            }
+            else
+            {
+                result = hexabc[decNumber % 16] + result;
+                break;
+            }
+        }
+        
+        return result;
     }
 }
